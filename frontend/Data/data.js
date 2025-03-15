4ff8a5915a87d1767b3e978afb338ab176ac51db
Aanchal <159253330+AchlMaurya@users.noreply.github.com>
2025-03-15 22:44:31 +0530

    Create data.js

---
+++ frontend/Data/data.js
@@ -0,0 +1,34 @@
+{
+    "store": [
+        {
+            "storeLocation": "Andheri",
+            "storeAddress": "L-56, DN Nagar c.s.t. Road",
+            "storeCity": "Mumbai",
+            "storePhone": 9234676887
+        },
+        {
+            "storeLocation": "Navi Mumbai",
+            "storeAddress": "Mahalaxmi Road, Mumbai- 400034",
+            "storeCity": "Mumbai",
+            "storePhone": 9234676227
+        },
+        {
+            "storeLocation": "Bhayandar",
+            "storeAddress": "Hanuman Nagar",
+            "storeCity": "Mumbai",
+            "storePhone": 9234576887
+        },
+        {
+            "storeLocation": "Laxman",
+            "storeAddress": "L-23, Marol Naka",
+            "storeCity": "Chennai",
+            "storePhone": 9234656887
+        },
+        {
+            "storeLocation": "Colaba",
+            "storeAddress": "Mohan Nagar Road",
+            "storeCity": "Mumbai",
+            "storePhone": 9234676887
+        }
+    ]
+}
