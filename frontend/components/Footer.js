553e64254a50c7b57bbd52e4a689b034337b8ffa
Aanchal <159253330+AchlMaurya@users.noreply.github.com>
2025-03-15 22:40:22 +0530

    Add files via upload

---
+++ frontend/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//import styles from "../styles/Dashboard.module.css";
+import styles from "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+   
+    <div  className={styles.footer}>
+            <footer>
+              2021 © Laundry
+            </footer>
+            </div>
+  )
+}
+export default Footer;
